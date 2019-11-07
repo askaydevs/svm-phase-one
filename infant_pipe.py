0659eee77c49f3f2989dc88638afa74fb0db8e28
askaydevs <sk_singh18@outlook.com>
2019-11-07 18:01:49 +0530

    N-Gram integration from command line

--- infant_pipe.py
+++ infant_pipe.py
@@ -10,46 +10,76 @@ import os
 from os import listdir
 from os.path import isfile, join
 import re
+import nltk
+import numpy as np
 import spacy
-from tika import parser
+from spacy.attrs import LOWER, POS, ENT_TYPE, IS_ALPHA
+from spacy.tokens import Doc
+from sys import argv
 
-path = 'Solverminds_Data/Sample'
+#path = 'Solverminds_Data/Sample'
 
 def pdf_extract(file, dir_path = 'Solverminds_Data/Sample'):
-    extract = join(dir_path, file)
-    file_data = parser.from_file(extract)
-    text = file_data['content'].strip()
-    #text = text[73:]
-    text = str(text.strip())
-    return text
+    text = ""
+    with open(join(dir_path, file), 'rb') as f:
+        pdf = PyPDF2.PdfFileReader(f)
+        for _ in range(0, pdf.getNumPages()):
+            page = pdf.getPage(_)
+            text += page.extractText()
+    return text.strip()
 #print(processed)
 def process(text):
-    processed = re.sub(r'[^\w\s]', ' ', text)
-    processed = text.replace('\n', ' ')
+    processed = text.replace('\n', ' ').replace('\r', '')
+    processed = re.sub("\s\s+", " ", processed)
     #processed = re.sub(' +', ' ', processed)
     processed = processed.strip()
     return processed
 
-def input_writer():
-    if not os.path.exists('test_input.txt'):
-        with open('test_input.txt', 'wb') as f:
-            f.write(b + processed)
-            print("Transformation done successfully!!\n")
-        f.close()
+def remove_tokens_on_match(doc):
+    indexes = []
+    for index, token in enumerate(doc):
+        if(token.pos_ in ('PUNCT', 'SYM')):
+            indexes.append(index)
+        elif(token.is_stop):
+            indexes.append(index)
+    np_array = doc.to_array([LOWER, POS, ENT_TYPE, IS_ALPHA])
+    np_array = np.delete(np_array, indexes, axis=0)
+    doc2 = Doc(doc.vocab, words=[t.text for i, t in enumerate(doc) if i not in indexes])
+    doc2.from_array([LOWER, POS, ENT_TYPE, IS_ALPHA], np_array)
+    return doc2
 
-def rem_hf_panama(fr_text): # TODO: def rem_hf_hongkong
-    fr_text = fr_text.strip()
-    return fr_text[172:len(fr_text)-260:1] # This setting is specific to panama documents
+def ngram_list(n, word_list, stop_word_list=None):
+        """
+        Generate ngrams with width n excluding those that are entirely formed of stop words
 
-def get_body(text):
-    #text = rem_hf_panama(text)
-    index = []
-    for _ in re.findall(' +', text):
-        index.append(text.find(_))
-    for i in range(0, len(index)):
-        if (index[i+1] > index[i]):
-            indice = index[i + 1]
-            break
-        else:
-            i += 1
-    return text[indice:len(text):1].strip()
+        Args:
+            n (int): i.e. 1, 2, 3...
+            word_list (list of str): list of words
+            stop_word_list (list of str, Optional): list of words that should be excluded while obtaining
+                                                    list of ngrams
+
+        Returns:
+            list of str: List of ngrams formed from the given word list except for those that have all their tokes in
+                         stop words list
+        """
+        stop_word_set = set(stop_word_list) if stop_word_list else []
+        all_ngrams = nltk.ngrams(word_list, n)
+        ngram_list = []
+        for ngram in all_ngrams:
+            lowered_ngram_tokens = map(lambda token: token.lower(), ngram)
+            if any(token not in stop_word_set for token in lowered_ngram_tokens):
+                ngram_list.append(' '.join(ngram))
+        return ngram_list
+
+file_name = argv[1]
+n = int(argv[2])
+
+nlp = spacy.load('en_core_web_sm')
+text = process(pdf_extract(file_name))
+doc = nlp(text)
+#print(remove_tokens_on_match(doc))
+token_list = []
+for token in doc:
+    token_list.append(token.text)
+
+print(ngram_list(n, token_list))
