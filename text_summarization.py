ae8edad4837be2d163beb64d1c673ac7b404a770
Shubham Singh <sk_singh18@outlook.com>
2019-11-11 14:26:48 +0530

    Statistcs based text summarization. Half way done.

---
+++ text_summarization.py
@@ -0,0 +1,38 @@
+import re
+from infant_pipe import process, pdf_extract
+from nltk import sent_tokenize as st
+document = pdf_extract('001.pdf')
+processed = process(document)
+#print(processed)
+
+import nltk
+from nltk import sent_tokenize
+stopwords = nltk.corpus.stopwords.words('english')
+word_frequencies = {}
+for word in nltk.word_tokenize(processed):
+    if word not in stopwords:
+        if word not in word_frequencies.keys():
+            word_frequencies[word] = 1
+        else:
+            word_frequencies[word] += 1
+
+maximum_frequency = max(word_frequencies.values())
+for word in word_frequencies.keys():
+    word_frequencies[word] = (word_frequencies[word]/maximum_frequency)
+
+sentence_list = sent_tokenize(processed)
+
+sentence_scores = {}
+for sent in sentence_list:
+    for word in nltk.word_tokenize(sent.lower()):
+        if word in word_frequencies.keys():
+            if len(sent.split(' ')) < 30:
+                if sent not in sentence_scores.keys():
+                    sentence_scores[sent] = word_frequencies[word]
+                else:
+                    sentence_scores[sent] += word_frequencies[word]
+
+import heapq
+summary_sentences = heapq.nlargest(7, sentence_scores, key=sentence_scores.get)
+summary = ' '.join(summary_sentences)
+print(summary)
